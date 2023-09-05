9e3a0e29ca414fddda3cf85924a4d1f5b5730f44
Gabriel Góes <matt_cariusp@hotmail.com>
2023-09-05 10:10:47 -0300

    first hour

--- basics/Program.cs
+++ basics/Program.cs
@@ -1,37 +1,111 @@
 ﻿
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 /*
 WriteLine and Readline
 */
-Console.WriteLine("What's your name?");
-string greetings = "Hello " + Console.ReadLine();
-Console.WriteLine(greetings);
+//Console.WriteLine("What's your name?");
+//string greetings = "Hello " + Console.ReadLine();
+//Console.WriteLine(greetings);
 
 /*
 Int and Double
 */
 
-int a = 10;
-double b = 2.14;
+//int a = 10;
+//double b = 2.14;
 
-Console.WriteLine(a+b);
+//Console.WriteLine(a+b);
 
 /*
 Math
 */
 
-int x = 10;
-double y = 2.75;
-double xPow = Math.Pow(x,2);
-double xsqrt = Math.Sqrt(x);
-double yfloor = Math.Floor(y);
-double yceil = Math.Ceiling(y);
+//int x = 10;
+//double y = 2.75;
+//double xPow = Math.Pow(x,2);
+//double xsqrt = Math.Sqrt(x);
+//double yfloor = Math.Floor(y);
+//double yceil = Math.Ceiling(y);
 
 
 
-Console.WriteLine(xPow);
-Console.WriteLine(xsqrt);
-Console.WriteLine(yfloor);
-Console.WriteLine(yceil);
+//Console.WriteLine(xPow);
+//Console.WriteLine(xsqrt);
+//Console.WriteLine(yfloor);
+//Console.WriteLine(yceil);
 
+/*
+Randoms
+*/
+
+//Random random = new Random();
+
+//var num = random.Next(1,100);
+//Console.WriteLine(num);
+
+/*
+string Methods
+*/
+
+//String fullName = "Matt-Cari";
+
+//fullName = fullName.ToUpper();
+//fullName = fullName.ToLower();
+
+//fullName = fullName.Replace("-", " ");
+
+//String userName = fullName.Insert(0,"@");
+//Console.WriteLine(userName.Length);
+
+//String firstName = fullName.Substring(0,4);
+//String lastName = fullName.Substring(5,4);
+//Console.WriteLine(firstName);
+//Console.WriteLine(lastName);
+
+/*
+if statements
+*/
+
+// Console.WriteLine("What's your age?");
+// int age = Convert.ToInt32(Console.ReadLine());
+
+// if(age >= 18){
+//     Console.WriteLine("you are an adult");
+// }else{
+//     Console.WriteLine("you're not an adult");
+// }
+
+/*
+switches
+*/
+
+// Console.WriteLine("what day is today?");
+// String day = Console.ReadLine();
+
+// switch(day){
+//     case "Monday":
+//     Console.WriteLine("It's monday");
+//     break;
+//     case "Tuesday":
+//     Console.WriteLine("It's Tuesday");
+//     break;
+//      case "Wednesday":
+//     Console.WriteLine("It's Wednesday");
+//     break;
+//      case "Thursday":
+//     Console.WriteLine("It's Thursday");
+//     break;
+//      case "Friday":
+//     Console.WriteLine("It's Friday");
+//     break;
+//      case "Saturday":
+//     Console.WriteLine("It's Saturday");
+//     break;
+//      case "Sunday":
+//     Console.WriteLine("It's Sunday");
+//     break;
+//     default:
+//     Console.WriteLine(day + " is not a day");
+//     break;
+// }
