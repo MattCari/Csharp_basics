8ea26ff3263926be1c52245051ba8001c817754c
Gabriel Góes <matt_cariusp@hotmail.com>
2023-09-05 09:21:30 -0300

    some primitives

---
+++ basics/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+/*
+WriteLine and Readline
+*/
+Console.WriteLine("What's your name?");
+string greetings = "Hello " + Console.ReadLine();
+Console.WriteLine(greetings);
+
+/*
+Int and Double
+*/
+
+int a = 10;
+double b = 2.14;
+
+Console.WriteLine(a+b);
+
+/*
+Math
+*/
+
+int x = 10;
+double y = 2.75;
+double xPow = Math.Pow(x,2);
+double xsqrt = Math.Sqrt(x);
+double yfloor = Math.Floor(y);
+double yceil = Math.Ceiling(y);
+
+
+
+Console.WriteLine(xPow);
+Console.WriteLine(xsqrt);
+Console.WriteLine(yfloor);
+Console.WriteLine(yceil);
+
